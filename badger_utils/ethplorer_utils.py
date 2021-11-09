8d5601082eeb8a700d72925e351229ea906502ef
SHAKOTN <jadecoresky@gmail.com>
2021-11-09 20:34:45 +0200

    Make limit param optional in top holders fetching function

--- badger_utils/ethplorer_utils.py
+++ badger_utils/ethplorer_utils.py
@@ -1,12 +1,13 @@
 from typing import Dict
+from typing import Optional
 
 import requests
 
 
-def get_top_token_holders(token_address: str) -> Dict:
+def get_top_token_holders(token_address: str, limit: Optional[int] = 50) -> Dict:
     holders_response = requests.get(
         f"https://api.ethplorer.io/getTopTokenHolders/{token_address}",
-        params={"apiKey": "freekey", "limit": "50"},
+        params={"apiKey": "freekey", "limit": str(limit)},
     )
     holders_response.raise_for_status()
     return holders_response.json()
